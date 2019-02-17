9c3fdf0007b1c54d2b23977748466e4dc822d35e
irongut <murray.dave@outlook.com>
2019-02-17 21:14:38 +0000

    different icons for each menu item

---
+++ XF40Demo/XF40Demo/Convertors/MenuItemToIcon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XF40Demo.Convertors
+{
+    internal class MenuItemToIcon : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return String.Empty;
+            }
+            switch (((string)value).Trim().ToLower())
+            {
+                case "home":
+                    return "resource://XF40Demo.Resources.empire.blue.svg";
+                case "settings":
+                    return "resource://XF40Demo.Resources.engineer.white.svg";
+                default:
+                    return String.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XF40Demo/XF40Demo/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("General", "RCS1079:Throwing of new NotImplementedException.", Justification = "<Pending>", Scope = "member", Target = "~M:XF40Demo.Convertors.MenuItemToIcon.ConvertBack(System.Object,System.Type,System.Object,System.Globalization.CultureInfo)~System.Object")]
+
