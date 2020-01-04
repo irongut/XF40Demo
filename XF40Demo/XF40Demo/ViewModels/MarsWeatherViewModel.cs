93bc1619745b63ea1db231a450dff660e15e3852
Irongut <murray.dave@outlook.com>
2020-01-04 21:55:29 +0000

    added initial framework for Mars Weather page #24

---
+++ XF40Demo/XF40Demo/ViewModels/MarsWeatherViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF40Demo.ViewModels
+{
+    public class MarsWeatherViewModel : BaseViewModel
+    {
+        #region Properties
+
+        #endregion
+
+        public MarsWeatherViewModel()
+        {
+
+        }
+
+        protected override void RefreshView()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XF40Demo/XF40Demo/Views/MarsWeatherPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using XF40Demo.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF40Demo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MarsWeatherPage : ContentPage
+    {
+        private readonly MarsWeatherViewModel vm = new MarsWeatherViewModel();
+
+        public MarsWeatherPage()
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+
+        protected override void OnAppearing()
+        {
+            vm.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            vm.OnDisappearing();
+        }
+    }
+}
