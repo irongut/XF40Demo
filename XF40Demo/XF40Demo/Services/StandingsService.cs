492b2c80c0f048d28d2d7dba6f30074b5b411b8f
Irongut <murray.dave@outlook.com>
2020-05-24 03:48:34 +0100

    change StandingsService to a singleton #79

--- XF40Demo/XF40Demo/Services/StandingsService.cs
+++ XF40Demo/XF40Demo/Services/StandingsService.cs
@@ -8,14 +8,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Globalization;
+using MonkeyCache.FileStore;
 
 namespace XF40Demo.Services
 {
-    public class StandingsService
+    public sealed class StandingsService
     {
+        private static readonly StandingsService instance = new StandingsService();
+
         private const string URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQNi9sSQ2ziF44YgUJ4La_NK4ARJhrNZCoemGOktbHfWOw-Q2qj7Oot8MM-9qURRUSGZDcru7Is0XvH/pub?&output=csv";
         private const string dataKey = "Standings";
         private const string lastUpdatedKey = "StandingsUpdated";
+
+        private List<PowerStanding> powerStandings;
+        private int cycle;
+        private DateTime lastUpdated;
+
+        private StandingsService()
+        {
+            lastUpdated = DateTime.MinValue;
+        }
+
+        public static StandingsService Instance()
+        {
+            return instance;
+        }
 
         #region Properties
 
@@ -33,16 +50,40 @@ namespace XF40Demo.Services
 
         public async Task<(List<PowerStanding> standings, string cycle, DateTime updated)> GetData(CancellationTokenSource cancelToken, bool ignoreCache = false)
         {
-            DateTime lastUpdated = DateTime.Now;
-            string csvText = String.Empty;
-            string cycle = String.Empty;
             TimeSpan expiry = TimeSpan.FromMinutes(5);
+            if ((powerStandings?.Any() == false) || (cycle != CycleService.CurrentCycle() && (lastUpdated + expiry < DateTime.Now)))
+            {
+                // download the standings
+                string csvText;
+                DownloadService downloadService = DownloadService.Instance();
+                (csvText, lastUpdated) = await downloadService.GetData(URL, dataKey, lastUpdatedKey, expiry, cancelToken, ignoreCache).ConfigureAwait(false);
 
-            // download the csv
-            DownloadService downloadService = DownloadService.Instance();
-            (csvText, lastUpdated) = await downloadService.GetData(URL, dataKey, lastUpdatedKey, expiry, cancelToken, ignoreCache).ConfigureAwait(false);
+                // parse the standings
+                powerStandings = ParseStandings(csvText);
+                if (!string.IsNullOrWhiteSpace(powerStandings[0].Cycle))
+                {
+                    int p = powerStandings[0].Cycle.IndexOf(" ") + 1;
+                    Int32.TryParse(powerStandings[0].Cycle.Substring(p, powerStandings[0].Cycle.Length - p), out cycle);
+                }
+                else
+                {
+                    cycle = 0;
+                }
 
-            // parse the csv
+                // cache till next cycle if updated
+                if (cycle == CycleService.CurrentCycle())
+                {
+                    expiry = CycleService.TimeTillTick();
+                    Barrel.Current.Add(dataKey, csvText, expiry);
+                    Barrel.Current.Add(lastUpdatedKey, lastUpdated.ToString(), expiry);
+                }
+            }
+            return (powerStandings, powerStandings[0].Cycle, lastUpdated);
+        }
+
+        private List<PowerStanding> ParseStandings(string csvText)
+        {
+            string cycleNo = string.Empty;
             List<PowerStanding> standingList = new List<PowerStanding>();
             CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
@@ -55,7 +96,7 @@ namespace XF40Demo.Services
                 {
                     if (i == 12)
                     {
-                        cycle = csv.GetField<string>(1);
+                        cycleNo = csv.GetField<string>(1);
                     }
                     else if (i > 0)
                     {
@@ -75,9 +116,9 @@ namespace XF40Demo.Services
             }
             foreach (PowerStanding item in standingList)
             {
-                item.Cycle = cycle; // has to be set here because cycle is on the last row
+                item.Cycle = cycleNo; // has to be set here because cycle is on the last row
             }
-            return (standingList.OrderBy(x => x.Position).ToList(), cycle, lastUpdated);
+            return standingList.OrderBy(x => x.Position).ToList();
         }
 
         private StandingChange ConvertChange(string change)
