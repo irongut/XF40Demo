9b3fa4bb86138f494bf14087655d1adb863fe188
Irongut <murray.dave@outlook.com>
2020-11-26 02:06:42 +0000

    Power Details change alt colour by Power #185

---
+++ XF40Demo/XF40Demo/Convertors/PowerToAltColour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XF40Demo.Converters
+{
+    internal class PowerToAltColour : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return Color.White;
+            }
+            switch (((string)value).Trim().ToLower())
+            {
+                case "delaine":
+                case "patreus":
+                case "mahon":
+                case "lyr":
+                case "antal":
+                    return Color.Black;
+                default:
+                    return Color.White;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
